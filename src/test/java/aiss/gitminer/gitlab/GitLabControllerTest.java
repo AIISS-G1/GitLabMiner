f3bcdc69feb01711e949bd3505fc337ec5702f63
mimbrero <mimbrero.alberto@gmail.com>
2023-05-14 20:03:12 +0200

    test: use MockMvc to test the controller

--- src/test/java/aiss/gitminer/gitlab/GitLabControllerTest.java
+++ src/test/java/aiss/gitminer/gitlab/GitLabControllerTest.java
@@ -1,60 +1,78 @@
 package aiss.gitminer.gitlab;
 
-import aiss.gitminer.exception.AuthenticationException;
 import aiss.gitminer.gitlab.service.ProjectService;
 import aiss.gitminer.model.Project;
 import aiss.gitminer.service.GitMinerService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
+@WebMvcTest(GitLabController.class)
+@Import(GitLabController.class)
 class GitLabControllerTest {
 
-    @InjectMocks private GitLabController gitHubController;
+    @Autowired MockMvc mockMvc;
 
-    @Mock private ProjectService projectService;
-    @Mock private GitMinerService gitMinerService;
+    @MockBean private ProjectService projectService;
+    @MockBean private GitMinerService gitMinerService;
 
-    @Test
-    void givenNoAuthorizationHeader_throwsAuthenticationExceptionUnauthorized() {
-        assertThatThrownBy(() -> gitHubController.get(null, "foo", 2, 5, 2))
-                .isInstanceOfSatisfying(
-                        AuthenticationException.class,
-                        exception -> assertThat(exception.getStatus()).isEqualTo(HttpStatus.UNAUTHORIZED)
-                );
+    @Configuration
+    public static class RestTemplateConfig {
+        @Bean
+        public RestTemplateBuilder restTemplateBuilder() {
+            return new RestTemplateBuilder();
+        }
     }
 
     @Test
-    void givenAuthenticationHeaderWithToken_whenCallingGet_projectServiceIsCalledWithThatToken() {
+    void givenNoAuthorizationHeader_respondsWithUnauthorized() throws Exception {
+        this.mockMvc.perform(get("/{id}", "foo", 2, 5, 2))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void givenAuthenticationHeaderWithToken_whenCallingGet_projectServiceIsCalledWithThatToken() throws Exception {
         ArgumentCaptor<String> tokenCapture = ArgumentCaptor.forClass(String.class);
-        when(projectService.findById(anyString(), anyInt(), anyInt(), anyInt(), tokenCapture.capture())).thenReturn(new Project());
+        when(this.projectService.findById(anyString(), anyInt(), anyInt(), anyInt(), tokenCapture.capture())).thenReturn(new Project());
 
         String token = UUID.randomUUID().toString();
-        gitHubController.get("Bearer " + token, "foo", 2, 5, 2);
+        MockHttpServletRequestBuilder request = get("/{id}", "foo", 2, 5, 2)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token);
 
+        this.mockMvc.perform(request);
         assertThat(tokenCapture.getValue()).isEqualTo(token);
     }
 
     @Test
-    void givenCorrectParameters_whenCallingCreate_gitMinerServiceUploadIsCalled() {
-        when(projectService.findById(anyString(), anyInt(), anyInt(), anyInt(), anyString())).thenReturn(new Project());
-        doNothing().when(gitMinerService).uploadProject(any(Project.class));
+    void givenAProject_whenCallingPost_gitMinerServiceUploadIsCalledWithThatProject() throws Exception {
+        Project dummyProject = new Project();
+
+        when(this.projectService.findById(anyString(), anyInt(), anyInt(), anyInt(), anyString())).thenReturn(dummyProject);
+        doNothing().when(gitMinerService).uploadProject(dummyProject);
 
         String token = UUID.randomUUID().toString();
-        gitHubController.create("Bearer " + token, "foo", 2, 5, 2);
+        MockHttpServletRequestBuilder request = post("/{id}", "foo", 2, 5, 2)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token);
 
-        verify(gitMinerService).uploadProject(any(Project.class));
+        this.mockMvc.perform(request);
+        verify(gitMinerService).uploadProject(dummyProject);
     }
 }
